fd8ff0e16ede3f09eb7fc186aab011cd01710d26
anuja-mishra <sharma.anuja88@gmail.com>
2019-02-21 15:32:14 +0530

    first file on git

---
+++ anuja_sharma_01.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 14 14:02:16 2018
+
+@author: sharm
+"""
+
+str1="""This is a multi line stirng. This code challenge is to 
+test your understanding about strings. You need to print  some part of this string.
+From here print this text without manually counting the indexes."""
+
+
+a=str1.index('F')
+
+print(str1[a:])
